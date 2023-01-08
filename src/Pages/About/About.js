bacfd969383ee605c81f6ed96884b0ee3247c9fb
MahadiMunna <mahadimunna.official@gmail.com>
2023-01-08 12:12:51 +0600

    about paragraph responsive

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -15,7 +15,7 @@ const About = () => {
                     </PhotoProvider>
                     <div className='lg:ml-10 lg:w-96'>
                         <h1 data-aos="fade-up" data-aos-duration="1500" data-aos-delay="200" className="text-4xl text-indigo-600 font-bold text-center lg:text-justify">About Me!</h1>
-                        <p data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" className="py-6 text-center lg:text-left">Currently I'am a student of Daffodil International University. I'm doing my BSc in Computer Science and Engineering. My graduation will be complete in 2024. I have learned full stack web development and now I want to engage my self with industries. Looking for internship/full time job.</p>
+                        <p  data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" className="py-6 md:w-96 lg:w-full text-center lg:text-left">Currently I'am a student of Daffodil International University. I'm doing my BSc in Computer Science and Engineering. My graduation will be complete in 2024. I have learned full stack web development and now I want to engage my self with industries. Looking for internship/full time job.</p>
                         
                     </div>
                 </div>
